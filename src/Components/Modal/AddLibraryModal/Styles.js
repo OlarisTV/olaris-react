242563e6ce4ff067504073e11b98c64110d9687c
Ben <ben@benclarke.me>
2019-06-17 20:11:27 +0100

    Added Library Type & healthy check to library list

--- src/Components/Modal/AddLibraryModal/Styles.js
+++ src/Components/Modal/AddLibraryModal/Styles.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { placeholder } from 'polished';
 
 export const SubmitLibrary = styled.button`
     font-size: 1.4rem;
@@ -42,19 +41,53 @@ export const LibraryItemWrap = styled.article`
     background: rgba(0, 0, 0, 0.2);
 `;
 
+export const LibraryUnhealthy = styled(FontAwesomeIcon)`
+    color: ${(props) => props.theme.alerts.error};
+    font-size: 1.2rem;
+    height: 4.5rem;
+    float: left;
+    margin-right: 1.5rem;
+    min-width: 2.5rem;
+    cursor: pointer;
+    padding: 1.6rem 0;
+    margin: 0 0 0 -1rem;
+}
+`;
+
 export const LibraryItemFilePath = styled.span`
-    padding: 1.3rem 1.5rem;
     line-height: 1.8;
     border-radius: 0.2rem;
     color: #737382;
     display: block;
     float: left;
-    font-size: 1.4rem;
+    font-size: 1.2rem;
     font-weight: 600;
     flex: 1;
     max-width: calc(100% - 5rem);
-    word-break: break-all;
-    word-break: break-word;
+    line-height: 4.5rem;
+    padding: 0 1.5rem;
+
+    p {
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+    }
+
+    span {
+        float: left;
+        padding: 0 1.5rem;
+        transform: translateX(-1.5rem);
+        color: #fff;
+        background: rgba(0, 0, 0, 0.1);
+        border-radius: 0.2rem 0 0.2rem 0;
+        border-right: 1px solid rgba(0, 0, 0, 0.3);
+        text-transform: capitalize;
+        max-width: 15rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        font-size: 1.2rem;
+    }
 `;
 
 export const LibraryItemDelete = styled(FontAwesomeIcon)`
